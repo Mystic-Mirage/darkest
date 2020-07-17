621186c4c3094a8b411fcaaf205e88370af189fb
Mystic-Mirage <mm@m10e.net>
2020-07-17 11:18:20 +0300

    Require darker >=1.0

--- darkest/format.py
+++ darkest/format.py
@@ -1,12 +1,9 @@
 from pathlib import Path
 from typing import List, Optional
 
-from darker.black_diff import (
-    diff_and_get_opcodes,
-    opcodes_to_chunks,
-    run_black,
-)
+from darker.black_diff import BlackArgs, run_black
 from darker.chooser import choose_lines
+from darker.diff import diff_and_get_opcodes, opcodes_to_chunks
 from darker.utils import joinlines
 from darker.verification import NotEquivalentError, verify_ast_unchanged
 
@@ -15,7 +12,10 @@ def format_file(
     src: Path, from_line: int, to_line: int, config: Optional[str],
 ) -> None:
     while True:
-        original, formatted = run_black(src, config)
+        black_args = BlackArgs(config=config)
+        src_contents = src.read_text()
+        original = src_contents.splitlines()
+        formatted = run_black(src, src_contents, black_args)
 
         opcodes = diff_and_get_opcodes(original, formatted)
         black_chunks = list(opcodes_to_chunks(opcodes, original, formatted))
@@ -29,7 +29,9 @@ def format_file(
         result_str = joinlines(chosen_lines)
 
         try:
-            verify_ast_unchanged(original, result_str, black_chunks, linenums)
+            verify_ast_unchanged(
+                src_contents, result_str, black_chunks, linenums
+            )
         except NotEquivalentError:
             from_line = max(1, from_line - 1)
             to_line = min(max_line, to_line + 1)
--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     entry_points={"console_scripts": ["darkest = darkest.__main__:main"]},
-    install_requires=["darker @ git+https://github.com/akaihola/darker.git",],
+    install_requires=["darker>=1.0"],
 )
