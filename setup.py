2ff67825ff00ebf799d60bff399938a3411f497f
Mystic-Mirage <mm@m10e.net>
2020-07-27 17:24:30 +0300

    Use setup.cfg

--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="darkest",
-    version="0.1.0",
-    packages=find_packages(),
-    entry_points={"console_scripts": ["darkest = darkest.__main__:main"]},
-    install_requires=["darker>=1.0"],
-)
+setup()
