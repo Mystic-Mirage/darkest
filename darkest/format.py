7f94920951d0b6f333ca8234cb3b42ebc5e5232c
Mystic-Mirage <mm@m10e.net>
2021-05-03 19:52:14 +0300

    Add support of darker >= 1.2.3

--- darkest/format.py
+++ darkest/format.py
@@ -1,11 +1,15 @@
 from pathlib import Path
-from typing import List
 
+from darker.__main__ import modify_file
 from darker.black_diff import BlackArgs, run_black
 from darker.chooser import choose_lines
 from darker.diff import diff_and_get_opcodes, opcodes_to_chunks
-from darker.utils import joinlines
-from darker.verification import NotEquivalentError, verify_ast_unchanged
+from darker.utils import TextDocument
+from darker.verification import (
+    BinarySearch,
+    NotEquivalentError,
+    verify_ast_unchanged,
+)
 
 
 def format_file(
@@ -13,34 +17,27 @@ def format_file(
     from_line: int,
     to_line: int,
     config: str = None,
-) -> None:
-    while True:
-        black_args = BlackArgs(config=config)
-        src_contents = src.read_text()
-        original = src_contents.splitlines()
-        formatted = run_black(src, src_contents, black_args)
+):
+    black_args = BlackArgs(config=config)
+    edited = worktree_content = TextDocument.from_file(src)
+    max_context_lines = len(edited.lines)
+    minimum_context_lines = BinarySearch(0, max_context_lines + 1)
+    edited_linenums = list(range(from_line, to_line + 1))
 
-        opcodes = diff_and_get_opcodes(original, formatted)
-        black_chunks = list(opcodes_to_chunks(opcodes, original, formatted))
-
-        max_line = len(original)
-        to_line = (to_line + 1) or max_line
-        linenums = list(range(from_line, to_line))
-
-        chosen_lines: List[str] = list(choose_lines(black_chunks, linenums))
-
-        result_str = joinlines(chosen_lines)
+    while not minimum_context_lines.found:
+        formatted = run_black(src, edited, black_args)
+        opcodes = diff_and_get_opcodes(edited, formatted)
+        black_chunks = list(opcodes_to_chunks(opcodes, edited, formatted))
+        chosen = TextDocument.from_lines(
+            choose_lines(black_chunks, edited_linenums),
+            encoding=worktree_content.encoding,
+            newline=worktree_content.newline,
+        )
 
         try:
-            verify_ast_unchanged(
-                src_contents, result_str, black_chunks, linenums
-            )
+            verify_ast_unchanged(edited, chosen, black_chunks, edited_linenums)
         except NotEquivalentError:
-            from_line = max(1, from_line - 1)
-            to_line = min(max_line, to_line + 1)
-            if from_line == 1 and to_line == max_line:
-                raise
-            continue
-
-        src.write_text(result_str)
-        break
+            minimum_context_lines.respond(False)
+        else:
+            minimum_context_lines.respond(True)
+            modify_file(src, chosen)
